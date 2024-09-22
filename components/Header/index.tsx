9ab68ffc6b81cda987d4da8aa308bbd256a1392c
ceydacnrdl <ceydacinardal07@gmail.com>
2024-09-23 02:31:30 +0300

    Add Hakkimizda component and update Header component

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,22 +1,27 @@
 import BrandIcon from "@/icons/BrandIcon";
+import Link from "next/link";
 import React from "react";
 
+const menuItems = ["Anasayfa", "Hakkimizda", "Projeler", "Kariyer"];
 export default function Header() {
   return (
-    <div className="relative px-5 bg-transparent text-white font-bold text-lg font-serif">
-      <div className="flex justify-between">
+    <div className="relative px-5 bg-transparent text-white font-medium text-lg font-serif">
+      <div className="grid grid-cols-3">
         <div>
           <BrandIcon />
         </div>
         <div className="flex justify-between items-center gap-10">
-          <div>Anasayfa</div>
-          <div>Hakkımızda</div>
-          <div>Projeler</div>
-          <div>Kariyer</div>
-          <div>
-            <div>Language Picker</div>
-          </div>
+          {menuItems.map((item, index) => (
+            <Link
+              key={index}
+              className="border-b-2"
+              href={item.toLocaleLowerCase()}
+            >
+              {item}
+            </Link>
+          ))}
         </div>
+        <div className="flex justify-end items-center">EN</div>
       </div>
     </div>
   );
