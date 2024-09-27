972a53c3abd3b7da34119432dcf9e87dc5ebd59c
ceydacnrdl <ceydacinardal07@gmail.com>
2024-09-28 00:13:46 +0300

    Refactor language handling, update environment variables, and improve layout styling

--- components/CareerCard/index.tsx
+++ components/CareerCard/index.tsx
@@ -23,11 +23,19 @@ export default function CareerPage({ data }: CareerCardProps) {
       </div>
       <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 text-black">
         {data.positions.map((position, index) => (
-          <div key={index} className="p-6 bg-gray-100 rounded-2xl shadow-md">
-            <h2 className="text-2xl font-bold mb-2">{position.title}</h2>
-            <p className="text-lg mb-2">{position.description}</p>
-            <h3 className="text-xl font-semibold">Requirements:</h3>
-            <p className="text-lg">{position.requirements}</p>
+          <div
+            key={index}
+            className="p-6 bg-gray-100 rounded-2xl shadow-md flex flex-col justify-between"
+          >
+            <div>
+              <h2 className="text-2xl font-bold mb-2">{position.title}</h2>
+              <p className="text-lg mb-2">{position.description}</p>
+              <h3 className="text-xl font-semibold">Requirements:</h3>
+              <p className="text-lg">{position.requirements}</p>
+            </div>
+            <button className="p-4 px-8 bg-customBlue text-white mt-4 self-start">
+              Başvur
+            </button>
           </div>
         ))}
       </section>
--- components/Header/LanguagePicker/index.tsx
+++ components/Header/LanguagePicker/index.tsx
@@ -1,37 +1,80 @@
+// "use client";
+
+// import { useRouter, usePathname } from "next/navigation";
+// import React from "react";
+// import ReactFlagsSelect from "react-flags-select";
+
+// const LanguagePicker = ({ currentLang }: { currentLang: string }) => {
+//   const languages = ["en", "tr", "ar", "de"];
+//   const router = useRouter();
+//   const pathname = usePathname();
+
+//   const handleLanguageChange = (
+//     event: React.ChangeEvent<HTMLSelectElement>
+//   ) => {
+//     const selectedLanguage = event.target.value;
+//     const newPathname = pathname.replace(
+//       `/${currentLang}`,
+//       `/${selectedLanguage}`
+//     );
+//     router.push(newPathname);
+//   };
+
+//   return (
+//     <div className="flex justify-end items-center text-black">
+//       <select
+//         onChange={handleLanguageChange}
+//         value={currentLang}
+//         className="p-2 bg-transparent text-white"
+//       >
+//         {languages.map((lang) => (
+//           <option key={lang} value={lang} className="text-black">
+//             {lang.toUpperCase()}
+//           </option>
+//         ))}
+//       </select>
+//     </div>
+//   );
+// };
+
+// export default LanguagePicker;
+
 "use client";
 
 import { useRouter, usePathname } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
+import ReactFlagsSelect from "react-flags-select";
 
 const LanguagePicker = ({ currentLang }: { currentLang: string }) => {
-  const languages = ["en", "tr", "ar", "de"];
+  const languages = ["US", "TR", "AR", "DE"];
   const router = useRouter();
   const pathname = usePathname();
+  const [selected, setSelected] = useState(currentLang.toUpperCase());
 
-  const handleLanguageChange = (
-    event: React.ChangeEvent<HTMLSelectElement>
-  ) => {
-    const selectedLanguage = event.target.value;
+  const handleLanguageChange = (code: string) => {
+    const selectedLanguage = code.toLowerCase();
     const newPathname = pathname.replace(
       `/${currentLang}`,
       `/${selectedLanguage}`
     );
     router.push(newPathname);
+    setSelected(code);
   };
 
   return (
-    <div className="flex justify-end items-center text-black">
-      <select
-        onChange={handleLanguageChange}
-        value={currentLang}
-        className="p-2 bg-transparent text-white"
-      >
-        {languages.map((lang) => (
-          <option key={lang} value={lang} className="text-black">
-            {lang.toUpperCase()}
-          </option>
-        ))}
-      </select>
+    <div className="flex justify-end items-center text-white">
+      <ReactFlagsSelect
+        countries={languages}
+        customLabels={{
+          US: "EN",
+          TR: "TR",
+          AR: "AR",
+          DE: "DE",
+        }}
+        selected={selected}
+        onSelect={handleLanguageChange}
+        className="p-2 bg-transparent text-black"
+      />
     </div>
   );
 };
